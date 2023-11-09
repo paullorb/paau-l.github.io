fc7d5eb6d72b659b96624ed2d34058b456b2dba2
Paul Lorber <paul.lorber@protonmail.com>
2023-11-09 20:05:18 +0100

    index.sql + app.js

--- sql/index.sql
+++ sql/index.sql
@@ -1,10 +1,4 @@
-CREATE TABLE personliches
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(255) NOT NULL,
-    lugar VARCHAR(255) NOT NULL,
-    nacionalidades VARCHAR(255) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    direccion VARCHAR(255) NOT NULL,
-    telefono VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-;
+CREATE DATABASE exampledb;
+CREATE USER 'polanks'@'localhost' IDENTIFIED BY 'polanks';
+GRANT ALL PRIVILEGES ON exampledb.* TO 'polanks'@'localhost';
+FLUSH PRIVILEGES;
