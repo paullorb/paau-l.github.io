dee6124a523f4e64f87dbf8aeb9783f22c71c70f
Paul Lorber <paul.lorber@protonmail.com>
2023-11-14 21:18:03 +0100

    babel code

--- js/cal.js
+++ js/cal.js
@@ -1,9 +1,35 @@
+const directions = { 0: 'top', 1: 'right', 2: 'bottom', 3: 'left' };
+
+const getDirectionKey = (ev, node) => {
+    const { width, height, top, left } = node.getBoundingClientRect();
+    const l = ev.clientX - (left + window.pageXOffset);
+    const t = ev.clientY - (top + window.pageYOffset);
+    const x = (l - (width / 2)) * (width > height ? (height / width) : 1);
+    const y = (t - (height / 2)) * (height > width ? (width / height) : 1);
+    return Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
+};
+
+class CellItem {
+    constructor(element) {
+        this.element = element;
+        this.element.addEventListener('mouseover', (ev) => this.update(ev, 'in'));
+        this.element.addEventListener('mouseout', (ev) => this.update(ev, 'out'));
+    }
+
+    update(ev, prefix) {
+        const direction = directions[getDirectionKey(ev, this.element)];
+        this.element.className = `cell ${prefix}-${direction}`;
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     // Table creation function
     function createTable(rows, cols) {
         let table = document.createElement('table');
         table.style.width = '100%';
         table.style.height = '100vh';
+        table.style.borderCollapse = 'collapse';
         table.setAttribute('border', '1');
     
         // Define an array of weekdays
@@ -18,26 +44,25 @@ document.addEventListener('DOMContentLoaded', function () {
     
             for (let j = 0; j < cols; j++) {
                 let td = tr.insertCell();
-    
-                // Check if it's the first row
+                td.classList.add('cell'); // Add class for styling
+        
+                // Create a div for the hover effect
+                let infoDiv = document.createElement('div');
+                infoDiv.classList.add('info');
+
+                // Add content to the div (you can customize this)
                 if (i === 0) {
-                    // Set the cell text to the weekday
-                    td.appendChild(document.createTextNode(weekdays[j % 7]));
+                    infoDiv.innerHTML = weekdays[j % 7];
                 } else {
-                    // For other rows, display Row and Column number
-                    td.appendChild(document.createTextNode(`Row ${i}, Column ${j + 1}`));
+                    infoDiv.innerHTML = `Row ${i}, Column ${j + 1}`;
                 }
+
+                // Append the div to the cell
+                td.appendChild(infoDiv);
     
                 td.style.textAlign = 'center';
                 td.style.height = cellHeight;
                 
-                // Add hover effect
-                td.onmouseover = function() {
-                    this.style.backgroundColor = 'gray'; // Change background color on hover
-                };
-                td.onmouseout = function() {
-                    this.style.backgroundColor = ''; // Revert background color when not hovering
-                };
             }
         }
     
